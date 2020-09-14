0f79a8990e7597e36af0a0f95e96b37ccb8ed775
xao-1269129591 <59197944+xao-1269129591@users.noreply.github.com>
2020-09-14 18:42:19 +0800

    阿里云短信服务更新

--- marvel-message-server/src/main/java/com/bim/marvel/feign/impl/AliSmsFeignControllerImpl.java
+++ marvel-message-server/src/main/java/com/bim/marvel/feign/impl/AliSmsFeignControllerImpl.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C), 2020, 安徽贝慕信息科技有限公司
  * FileName: AliSmsFeignControllerImpl
- * Author:   Administrator
+ * Author:   xao
  * Date:     2020/9/11 15:08
  * Description: 阿里云短信平台controller
  * History:
@@ -13,43 +13,17 @@ package com.bim.marvel.feign.impl;
 import com.bim.marvel.feign.AliSmsFeignController;
 import com.bim.marvel.message.sms.dto.AliSmsNoticeDTO;
 import com.bim.marvel.message.sms.dto.AliSmsValidCodeDTO;
-import com.bim.marvel.message.sms.entity.AliSmsConfig;
 import com.bim.marvel.message.sms.util.AliSmsUtil;
-import org.springframework.beans.factory.annotation.Value;
 import javax.validation.Valid;
 
 /**
  * 〈阿里云短信平台controller〉
  *
  * @author xao
- * @date 2020/9/11
+ * @date 2020/9/14
  * @since 1.0.0
  */
 public class AliSmsFeignControllerImpl implements AliSmsFeignController {
-
-    /**
-     * 阿里云accessKey
-     */
-    @Value("sms.aliSms.accessKey")
-    public String aliSmsAccessKey;
-
-    /**
-     * 阿里云accessSecret
-     */
-    @Value("sms.aliSms.accessSecret")
-    public String aliSmsAccessSecret;
-
-    /**
-     * 阿里云aliSmsConfig
-     */
-    public static AliSmsConfig aliSmsConfig = null;
-
-    /**
-     * AliSmsFeignControllerImpl
-     */
-    public AliSmsFeignControllerImpl(){
-        aliSmsConfig = new AliSmsConfig(aliSmsAccessKey, aliSmsAccessSecret);
-    }
 
     /**
      * 发送短信
