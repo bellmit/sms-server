27118be8346c65f6609ec2941bab85af0c7cef36
xao-1269129591 <59197944+xao-1269129591@users.noreply.github.com>
2020-09-16 16:52:13 +0800

    更新短信服务配置，完善短信服务日志，添加发送短信重试处理

--- marvel-message-server/src/main/java/com/bim/marvel/feign/AliSmsFeignClient.java
+++ marvel-message-server/src/main/java/com/bim/marvel/feign/AliSmsFeignClient.java
@@ -1,8 +1,8 @@
 /*
  * Copyright (C), 2020, 安徽贝慕信息科技有限公司
  * FileName: AliSmsFeignClient
- * Author:   Administrator
- * Date:     2020/9/11 17:31
+ * Author:   xao
+ * Date:     2020/9/16
  * Description: 阿里短信接口
  * History:
  * <author>          <time>          <version>          <desc>
@@ -10,15 +10,15 @@
  */
 package com.bim.marvel.feign;
 
-import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 〈阿里短信接口〉
  *
  * @author xao
- * @date 2020/9/11
+ * @date 2020/9/16
  * @since 1.0.0
  */
 @FeignClient(name = "aliSms", url = "http://dysmsapi.aliyuncs.com")
@@ -27,10 +27,10 @@ public interface AliSmsFeignClient {
     /**
      * 发送短信Feign
      *
-     * @param signature
-     * @param param1
-     * @param param2
+     * @param signature 短信签名
+     * @param param 短信参数
      */
-    @GetMapping("?Signature={signature}&param1={param1}&param2={param2}")
-    void sendAliSms(@Param("Signature") String signature, @Param("param1") String param1, @Param("param2") String param2);
+    @GetMapping
+    void sendAliSms(@RequestParam("Signature") String signature,
+                    @RequestParam("param") String param);
 }
